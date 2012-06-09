a1e6ad3e9591ce836789389e2b936b3f6a7bc56a
Drew Baumann <drewbaumann@gmail.com>
2012-06-09 16:53:30 -0700

    added img_url field

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,5 +4,6 @@ class Playlist
   field :url, :type => String
   field :source, :type => String
   field :playlist_id, :type => String
+  field :image_url, :type => String
   has_and_belongs_to_many :users
 end
