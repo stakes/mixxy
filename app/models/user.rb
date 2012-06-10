aa3d9fcbe6c9a56279b5771e6c2f00f4a9451918
Drew Baumann <drewbaumann@gmail.com>
2012-06-10 16:58:46 -0700

    deep… can't use current_user in the model!

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User
 
     p = Playlist.where(url: url).first
     p = Playlist.create(source: source, url: url, image_url: image_url, name: name) if p.blank?
-    @graph = Koala::Facebook::GraphAPI.new(current_user.get_auth('facebook').token)
+    @graph = Koala::Facebook::GraphAPI.new(self.get_auth('facebook').token)
     @graph.put_connections("me", "mixxyapp:star", :playlist => "http://mixxy.co/playlists/#{p.id.to_s}")
 
     self.likes << p.id if !self.likes.include?(p.id)
@@ -53,7 +53,7 @@ class User
     else
       p = like_playlist(source, url, image_url, name)
     end
-    @graph = Koala::Facebook::GraphAPI.new(current_user.get_auth('facebook').token)
+    @graph = Koala::Facebook::GraphAPI.new(self.get_auth('facebook').token)
     @graph.put_connections("me", "mixxyapp:add", :playlist => "http://mixxy.co/playlists/#{p.id.to_s}")
     
     self.save
