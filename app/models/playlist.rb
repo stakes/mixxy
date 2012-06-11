a269ab4ae2f2a190a88c5543a119e4ae39a9d789
Jay Stakelon <stakelon@gmail.com>
2012-06-10 18:07:57 -0700

    limit sc again

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -11,7 +11,7 @@ class Playlist
   def self.get_sc_followings_playlists(user_id)
     client = Soundcloud.new(:access_token => User.find(user_id).get_auth('soundcloud').token)
     playlist_array = []
-    followings = client.get("/me/followings")
+    followings = client.get("/me/followings", :limit => 5)
     followings.each do |f|
       if f.playlist_count > 0
         playlists = client.get("/users/#{f.id}/playlists", :limit => 2)
